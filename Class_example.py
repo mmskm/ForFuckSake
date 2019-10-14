a134704544e5ca6481eb97ea133c36e5dc37b628
mmskm <mmskm911@gmail.com>
2019-10-14 18:53:42 -0500

    2

--- Class_example.py
+++ Class_example.py
@@ -11,6 +11,12 @@ class TwoNumbers() :
     def Divide(self) :
         print(self.number_1, ' / ',self.number_2, ' = ',self.number_1/self.number_2)   
 
+class TimesTable():
+    def __init__(self,a):
+        self.number = a
+    def PrintTable(self) :
+        for index in range(10) :
+            print(self.number, ' * ', index+1,' = ', self.number*(index+1))
 
 
 Two_numbers = TwoNumbers(10,5)
@@ -19,3 +25,6 @@ Two_numbers.Multiply()
 Two_numbers.Minus()
 Two_numbers.Plus()
 Two_numbers.Divide()
+
+Number = TimesTable(9)
+Number.PrintTable()
