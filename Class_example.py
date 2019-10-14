f150ff9f1a9336912534dadcecdbccdb0ede5fdb
mmskm <mmskm911@gmail.com>
2019-10-14 18:45:18 -0500

    2

---
+++ Class_example.py
@@ -0,0 +1,19 @@
+class TwoNumbers() :
+    def __init__(self, a , b):
+        self.number_1 = a
+        self.number_2 = b
+    def Multiply(self) :
+        print(self.number_1, ' * ',self.number_2, ' = ',self.number_1*self.number_2)    
+    def Plus(self) :
+        print(self.number_1, ' + ',self.number_2, ' = ',self.number_1+self.number_2)   
+    def Minus(self) :
+        print(self.number_1, ' - ',self.number_2, ' = ',self.number_1-self.number_2)           
+    def Divide(self) :
+        print(self.number_1, ' / ',self.number_2, ' = ',self.number_1/self.number_2)   
+
+Two_numbers = TwoNumbers(10,5)
+
+Two_numbers.Multiply
+Two_numbers.Minus
+Two_numbers.Plus
+Two_numbers.Divide
